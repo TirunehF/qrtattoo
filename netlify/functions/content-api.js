b17848aa09daa74b3c64dc76acfb6a80b35337e5
Fikru T <fikruxd@gmail.com>
2024-08-10 10:28:15 +0200

    functions changed

--- netlify/functions/content-api.js
+++ netlify/functions/content-api.js
@@ -1,12 +1,4 @@
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "This is a test message from your Netlify Function!" })
-  };
-};
-
-// In content-api.js
-exports.handler = async function(event, context) {
   if (event.httpMethod === 'POST') {
     const { type, value } = JSON.parse(event.body);
     // Save the content type and value to your configuration storage (could be a file for simplicity)
